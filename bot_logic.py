763ee61a0cbaccc0dc610715c1bd252c6599f9d0
superjuegoloco70 <141503162+superjuegoloco70@users.noreply.github.com>
2023-08-26 12:34:19 -0300

    Add files via upload

---
+++ bot_logic.py
@@ -0,0 +1,21 @@
+import random
+
+def gen_pass(pass_length):
+    elements = "abcdefghijklmnoprstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+def coin_flip():
+    side = random.randint(1,2)
+    if side == 1:
+        return "Heads"
+    elif side == 2:
+        return "Tails"
+    
+def help():
+    commands = "Flip a coin, Generate a password, Help, deleteme"
+    return "Commands: "+commands
---
+++ main.py
@@ -0,0 +1,33 @@
+import discord
+from bot_logic import *
+
+# La variable intents almacena los privilegios del bot
+intents = discord.Intents.default()
+# Activar el privilegio de lectura de mensajes
+intents.message_content = True
+# Crear un bot en la variable cliente y transferirle los privilegios
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\\U0001f642")
+    elif message.content.startswith("$Flip a coin"):
+        await message.channel.send(coin_flip())
+    elif message.content.startswith("$Generate a password"):
+        await message.channel.send(gen_pass(10))    
+    elif message.content.startswith("$Help"):
+        await message.channel.send(help())
+    elif message.content.startswith('$deleteme'):
+            msg = await message.channel.send('I will delete myself now...')
+            await msg.delete()  
+    
+client.run("")
